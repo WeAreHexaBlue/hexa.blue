5ea9144691aa8facaec97339b9408a70f066915b
ActuallyFlamey <flamey@hexa.blue>
2026-08-29 13:04:37 +0200

    change: better footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -10,7 +10,8 @@
         <img src={logoWhite} class="size-15" alt="HexaBlue Logo" />
         <h1 class="title white">{m.hexablue()}</h1>
     </a>
-    <p class="text-gray-300">{m.footer_desc()}</p>
+    <p>{@html m.footer_desc()}</p>
+    <p>{@html m.footer_joindc({ href: "https://discord.gg/DsARcGwwdM" })}</p>
 </div>
 
 <style lang="postcss">
@@ -19,6 +20,7 @@
     .footer {
         @apply absolute grid mx-auto w-full bottom-0;
         @apply justify-items-center gap-y-2;
-        @apply bg-mist-800 px-6 md:px-0; py-6 rounded-t-4xl;
+        @apply bg-mist-800 px-6 md:px-0 py-6 rounded-t-4xl;
+        @apply text-gray-300;
     }
 </style>
