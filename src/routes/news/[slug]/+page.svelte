fe936a2b4161f315728b259d46a9449773901414
ActuallyFlamey <flamey@hexa.blue>
2026-08-30 20:42:57 +0200

    feat: single article view

--- src/routes/news/[slug]/+page.svelte
+++ src/routes/news/[slug]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { resolve } from "$app/paths";
     import { m } from "$lib/paraglide/messages";
     import { formatDateTime } from "$lib";
 
@@ -13,5 +14,29 @@
     <title>{m["news.slug.title"]({ title: article.title })}</title>
 </svelte:head>
 <main>
-    
+    <h1 class="text-5xl font-extrabold">{article.title}</h1>
+    <sub>
+        {@html sanitize(
+            m["news.published"](
+                { author: article.author, ...formatDateTime(article.created_at) }
+            ), {
+                allowedTags: ["b", "i"],
+                allowedAttributes: {}
+            }
+        )}
+    </sub>
+    <p class="mx-auto my-8 inset-x-0 max-w-prose">
+        {@html sanitize(
+            snarkdown(article.body),
+            {
+                allowedTags: ["b", "i", "u", "em", "strong", "a"],
+                allowedAttributes: {
+                    "a": [ "href", "class" ]
+                }
+            }
+        )}
+    </p>
+    <a href={resolve("/news")} class="hover:gradtxt underline text-2xl">
+        {m["news.slug.back"]()}
+    </a>
 </main>
